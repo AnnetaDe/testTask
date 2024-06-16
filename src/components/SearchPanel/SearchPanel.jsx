ba7a88f92d09696ca0ec7fdcd426aa4a8c81b653
AnnetaDe <anneta.liss@gmail.com>
2024-06-17 01:05:04 +0200

    4

--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -10,14 +10,14 @@ import {
   setToFilter,
 } from '../../redux/filter/filtersSlice';
 import { formatMakes, formatMileage, formatPrice, unic } from '../../helpers';
-import { searchPanelFromToStyles, searchPanelStyles } from './searchPanelStyles';
+import { from, searchPanelStyles, to } from './searchPanelStyles';
 import { useState } from 'react';
 
 export const SearchPanel = () => {
   const dispatch = useDispatch();
   const cars = useSelector(selectAllCars);
-  const [kmFrom, setKmFrom] = useState([]);
-  const [kmTo, setKmTo] = useState([]);
+  const [kmFrom, setKmFrom] = useState('');
+  const [kmTo, setKmTo] = useState('');
 
   const makes = cars.map(car => ({ value: car.make, label: car.make }));
 
@@ -32,48 +32,59 @@ export const SearchPanel = () => {
   console.log('makes', makes);
   return (
     <div className={s.searchPanel}>
-      <label htmlFor="brand">Car brand</label>
-      <Select
-        inputId="brand"
-        classNames={s.select}
-        styles={searchPanelStyles}
-        className={s.select}
-        options={unicMake}
-        onChange={option => dispatch(setBrandFilter(option.value))}
-        isSearchable={true}
-        placeholder="Search by brand"
-      />
-      <label htmlFor="price">Price/ 1 hour</label>
-      <Select
-        inputId="price"
-        classNames={s.select}
-        styles={searchPanelStyles}
-        options={unicPrice}
-        onChange={option => dispatch(setPriceFilter(option.value))}
-        defaultValue={unicPrice[0]}
-        isSearchable={true}
-      />
-      <div className={s.fromTo}>
-        <label htmlFor="from">Сar mileage / km</label>
+      <div>
+        <label htmlFor="brand">Car brand</label>
         <Select
-          inputId="from"
-          classNames={s.selectFrom}
-          styles={searchPanelFromToStyles}
-          options={unicMileage}
-          onChange={option => dispatch(setFromFilter(option.value)) | setKmFrom(option.value)}
-          placeholder="From"
-          controlShouldRenderValue={true}
-          isOptionDisabled={option => option.value >= kmTo && option.value !== '' && kmTo !== ''}
+          inputId="brand"
+          classNames={s.select}
+          styles={searchPanelStyles}
+          className={s.select}
+          options={unicMake}
+          onChange={option => dispatch(setBrandFilter(option.value))}
           isSearchable={true}
+          placeholder="Search by brand"
         />
+      </div>
+      <div>
+        <label htmlFor="price">Price/ 1 hour</label>
         <Select
-          classNames={s.selectTo}
-          styles={searchPanelFromToStyles}
-          options={unicMileage}
-          onChange={option => dispatch(setToFilter(option.value)) | setKmTo(option.value)}
-          placeholder="To"
-          isOptionDisabled={option => option.value <= kmFrom && option.value !== ''}
+          inputId="price"
+          classNames={s.select}
+          styles={searchPanelStyles}
+          options={unicPrice}
+          onChange={option => dispatch(setPriceFilter(option.value))}
+          isSearchable={true}
+          placeholder="Search by price"
         />
+      </div>
+      <div className={s.fromTo}>
+        <div>
+          <label htmlFor="from">Сar mileage / km</label>
+          <div className={s.fromToSelectors}>
+            <Select
+              inputId="from"
+              classNames={s.selectFrom}
+              styles={from}
+              options={unicMileage}
+              onChange={option => dispatch(setFromFilter(option.value)) | setKmFrom(option.value)}
+              placeholder="From"
+              controlShouldRenderValue={true}
+              isOptionDisabled={option =>
+                option.value >= kmTo && option.value !== '' && kmTo !== ''
+              }
+              isSearchable={true}
+            />
+
+            <Select
+              classNames={s.selectTo}
+              styles={to}
+              options={unicMileage}
+              onChange={option => dispatch(setToFilter(option.value)) | setKmTo(option.value)}
+              placeholder="To"
+              isOptionDisabled={option => option.value <= kmFrom && option.value !== ''}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SearchPanel/searchPanelStyles.js
+++ src/components/SearchPanel/searchPanelStyles.js
@@ -3,7 +3,7 @@ import { SearchPanel } from './SearchPanel';
 export const searchPanelStyles = {
   control: (baseStyles, state) => ({
     ...baseStyles,
-    borderColor: state.isFocused ? 'grey' : ' rgba(74, 86, 226, 0.10)',
+    borderColor: state.isFocused ? 'grey' : ' #F7F7FB',
     backgroundColor: '#F7F7FB',
     color: '#121417',
     borderCollapse: 'separate',
@@ -48,13 +48,15 @@ export const searchPanelStyles = {
   cursor: 'pointer',
 };
 
-export const searchPanelFromToStyles = {
+export const from = {
   control: (baseStyles, state) => ({
     ...baseStyles,
+    borderColor: state.isFocused ? 'grey' : ' #F7F7FB',
+
     width: '160px',
     backgroundColor: '#F7F7FB',
     borderCollapse: 'collapse',
-    borderRadius: '14px',
+    borderRadius: '14px 0 0 14px',
   }),
   dropdownIndicator: baseStyles => ({
     ...baseStyles,
@@ -65,6 +67,54 @@ export const searchPanelFromToStyles = {
     padding: '8px',
     background: '#fff',
     borderCollapse: 'separate',
+    borderRadius: '14px',
+    scrollBehavior: 'smooth',
+    fontSize: '16px',
+  }),
+  option: (baseStyles, state) => ({
+    ...baseStyles,
+    backgroundColor: state.isFocused ? '#ffffff1a' : 'transparent',
+    color: state.isSelected ? '#121417' : '#12141733',
+    cursor: state.isDisabled ? 'not-allowed' : 'pointer',
+    RxFontStyle: state.isSelected ? 'bold' : 'normal',
+  }),
+  singleValue: baseStyles => ({
+    ...baseStyles,
+    color: '#121417',
+    fontSize: '18px',
+    fontWeight: '500',
+  }),
+
+  indicatorSeparator: baseStyles => ({
+    ...baseStyles,
+    display: 'none',
+  }),
+  valueContainer: baseStyles => ({
+    ...baseStyles,
+    padding: '14px 14px 14px 18px',
+  }),
+
+  cursor: 'pointer',
+};
+export const to = {
+  control: (baseStyles, state) => ({
+    ...baseStyles,
+    borderColor: state.isFocused ? 'grey' : ' #F7F7FB',
+    borderLeft: '1px solid rgba(138, 138, 137, 0.20)',
+
+    width: '160px',
+    backgroundColor: '#F7F7FB',
+    borderRadius: '0 14px 14px 0',
+  }),
+  dropdownIndicator: baseStyles => ({
+    ...baseStyles,
+    color: '#121417',
+  }),
+  menu: baseStyles => ({
+    ...baseStyles,
+    padding: '8px',
+    background: '#fff',
+    borderCollapse: 'collapse',
     borderRadius: '14px',
     scrollBehavior: 'smooth',
     fontSize: '16px',
