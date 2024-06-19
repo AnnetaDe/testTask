953831242145e227fc4aea8981bb32126dbc76b5
AnnetaDe <anneta.liss@gmail.com>
2024-06-19 19:15:01 +0200

    routes
    added suspenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css';
 
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout/Layout';
 import { NotFound } from './pages/NotFound/NotFound.jsx';
-import { Home } from './pages/Home';
-import { Catalog } from './pages/Cars/Catalog.jsx';
-import { Favorites } from './pages/Favorites/Favorites.jsx';
 import { useDispatch } from 'react-redux';
 import { lazy, Suspense, useEffect } from 'react';
 import { getAll, getDaily } from './redux/cars/operations.js';
 import { Loader } from './components/Loader/Loader.jsx';
 const Home = lazy(() => import('./pages/Home'));
-const Catalog = lazy(() => import('./pages/Cars/Catalog.jsx'));
+const Catalog = lazy(() => import('./pages/Catalog/Catalog.jsx'));
 const Favorites = lazy(() => import('./pages/Favorites/Favorites.jsx'));
 
 function App() {
