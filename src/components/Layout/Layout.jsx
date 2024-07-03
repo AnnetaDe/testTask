7b6875de19a62faefa33df2b43f7a1ba8d18ffde
AnnetaDe <anneta.liss@gmail.com>
2024-07-04 00:42:11 +0200

    container

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import { NavBar } from '../NavBar/NavBar';
 import s from './Layout.module.css';
+import { Container } from '../Container/Container';
 
 export const Layout = () => {
   return (
-    <div className={s.layout}>
-      <header>
-        <p> {''}</p>
-        <NavBar />
-      </header>
-      <main className={s.main}>
-        <Outlet />
-      </main>
-    </div>
+    <Container>
+      <div className={s.layout}>
+        <header>
+          <p> {''}</p>
+          <NavBar />
+        </header>
+        <main className={s.main}>
+          <Outlet />
+        </main>
+      </div>
+    </Container>
   );
 };
